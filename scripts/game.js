ff853141bb13c4186ea3fbac8ca1c06ff5ebd321
DaceMoria <leo.arthaud@epsi.fr>
2020-01-12 15:03:54 +0100

    init game.js

---
+++ scripts/game.js
@@ -0,0 +1,26 @@
+'use strict';
+
+let game;
+
+window.onload = function() {
+
+
+};
+
+
+// playGame scene
+class playGame extends Phaser.Scene{
+
+    preload(){
+       
+
+    }
+    create(){
+
+    }
+
+    update(){
+
+    }
+
+}

c2054790f153439af5834b9926d610d963a34ef1
Mehcheh <macha.jumelin@epsi.fr>
2020-01-12 15:07:47 +0100

    preload ressources

--- scripts/game.js
+++ scripts/game.js
@@ -12,7 +12,13 @@ window.onload = function() {
 class playGame extends Phaser.Scene{
 
     preload(){
+        this.load.image("platformWhite", "ressources/graphics/platformWhite.png");
+        this.load.image("platformBlack", "ressources/graphics/platformBlack.png");
+        this.load.image("player", "ressources/graphics/player.png");
+        this.load.audio("gameover", 'ressources/sound/gameover.wav');
+        this.load.audio("jump", 'ressources/sound/jump.wav');
        
+    }
 
     }
     create(){
